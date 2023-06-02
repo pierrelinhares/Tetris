c19f550a7bbca853374543c417a4874ae96ab10c
Pedro Linhares <plinhares2004@gmail.com>
2023-06-02 03:55:47 +0000

    MainWindow.xaml.cs (1/2 done)

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -20,9 +20,62 @@ namespace Tetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ImageSource[] tilesImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileRed.png", UriKind.Relative))
+        };
+
+        private readonly ImageSource[] blockImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/BlocK-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/BlocK-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/BlocK-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/BlocK-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/BlocK-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/BlocK-Z.png", UriKind.Relative))
+        };
+
+        private readonly Image[,] imageControls;
+
+        private GameState gameState = new GameState();
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private Image[,] SetupGameCanvas(GameGrid grid)
+        {
+            Image[,] imageControls = new Image[grid.Rows, grid.Columns];
+            int cellsize = 25;
+
+            for (int r = 0; r < grid.Rows; r++)
+            {
+                for (int c = 0; c < grid.Columns; c++)
+                {
+                    Image imageControl = new Image
+                    {
+                        Width = cellsize,
+                        Height = cellsize,
+                    };
+
+                    Canvas.SetTop(imageControl, (r - 2) * cellsize);
+                    Canvas.SetLeft(imageControl, c * cellsize);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+            }
+
+            return imageControls;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
